c21957ed0da3d0ffab18862d7b0d6654ab8bbc17
Sil <silvanderkooi@gmail.com>
2019-04-09 10:08:13 +0200

    Muis Update

---
+++ Assets/Scripts/MousePosition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite mouseClickSprite, mouseIdleSprite;
+
+
+    void Start()
+    {
+        Cursor.visible = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            spriteRenderer.sprite = mouseClickSprite;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            spriteRenderer.sprite = mouseIdleSprite;
+        }
+    }
+}
