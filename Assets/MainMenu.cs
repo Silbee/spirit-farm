1761d54f7556e824de02b61d9f073e9a835522aa
MurkjeWasTakenSomeHowSoNowThisIsMyName <49203335+MurkjeWasTakenSomeHowSoNowThisIsMyName@users.noreply.github.com>
2019-04-09 10:23:12 +0200

    gae
    big geg

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
