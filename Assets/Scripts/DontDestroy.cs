7fa9bc617a9f62c61286c0d1bda5bd75f2322c12
Sil <silvanderkooi@gmail.com>
2019-04-10 13:44:55 +0200

    Upgrade naar 3.12f

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    static GameObject instance;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if(instance == null)
+        {
+            instance = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
